b607c385946b2c53afadceb6dbbf5d016c9fb30b
Alena Ruprecht <alena@brandwatch.com>
2014-02-25 10:42:12 +0000

    added food blogs modal content

--- js/FoodView/FoodBlogsView.js
+++ js/FoodView/FoodBlogsView.js
@@ -11,36 +11,34 @@ function(
         className: 'FoodBlogsView',
 
         render: function(){
-            var template = '<section class="section left">' +
-                                '<p class="market-title">Hisbe</p>' +
-                                '<p class="market-time"><i class="section-icon fa fa-clock-o"></i>Monday - Saturday 9am - 7.30pm & Sunday 10am - 4pm</p>' +
-                                '<a class="market-location" target="_blank" href="http://goo.gl/maps/87Ue3">' +
-                                    '<i class="section-icon fa fa-crosshairs"></i>Open Map</a>' +
-                                '<a class="market-info" target="_blank" href="http://hisbe.co.uk/">' + 
+            var template = '<section class="section small left">' +
+                                '<p class="section-title">Zero Waste Home Recipes</p>' +
+                                '<a class="section-info" target="_blank" href="http://goo.gl/lXmdQv">' + 
                                     '<i class="section-icon fa fa-file-o"></i>Visit Website</a>' +
                            '</section>' +
-                           '<section class="section right">' +
-                                '<p class="market-title">Ecostream</p>' +
-                                '<p class="market-time"><i class="section-icon fa fa-clock-o"></i>Monday - Friday 9am - 7pm & Weekends 10am - 5pm</p>' +
-                                '<a class="market-location" target="_blank" href="http://goo.gl/maps/bqbG9">' +
-                                    '<i class="section-icon fa fa-crosshairs"></i>Open Map</a>' +
-                                '<a class="market-info" target="_blank" href="http://www.ecostreamstore.com/">' + 
+                           '<section class="section small right">' +
+                                '<p class="section-title">Deliciously Organic</p>' +
+                                '<a class="section-info" target="_blank" href="http://deliciouslyorganic.net/">' + 
                                     '<i class="section-icon fa fa-file-o"></i>Visit Website</a>' +
                            '</section>' +
-                           '<section class="section left">' +
-                                '<p class="market-title">Taj The Grocer</p>' +
-                                '<p class="market-time"><i class="section-icon fa fa-clock-o"></i>Monday - Sunday 8.30am - 21.30pm</p>' +
-                                '<a class="market-location" target="_blank" href="http://goo.gl/maps/Cu8B9">' +
-                                    '<i class="section-icon fa fa-crosshairs"></i>Open Map</a>' +
-                                '<a class="market-info" target="_blank" href="http://goo.gl/EX3Qk5">' + 
+                           '<section class="section small left">' +
+                                '<p class="section-title">Easy Peasy Organic</p>' +
+                                '<a class="section-info" target="_blank" href="http://www.easypeasyorganic.com/">' + 
                                     '<i class="section-icon fa fa-file-o"></i>Visit Website</a>' +
                            '</section>' +
-                           '<section class="section right">' +
-                                '<p class="market-title">Inifinty Foods Wholesale</p>' +
-                                '<p class="market-time"><i class="section-icon fa fa-clock-o"></i>Monday - Friday 8.30am - 5pm</p>' +
-                                '<span class="market-location">' +
-                                    '<i class="section-icon fa fa-crosshairs"></i>Delivery Only</span>' +
-                                '<a class="market-info" target="_blank" href="http://www.infinityfoodswholesale.co.uk/">' + 
+                           '<section class="section small right">' +
+                                '<p class="section-title">The Organic Food Blog</p>' +
+                                '<a class="section-info" target="_blank" href="http://www.organicangels.com/blog/">' + 
+                                    '<i class="section-icon fa fa-file-o"></i>Visit Website</a>' +
+                           '</section>' +
+                           '<section class="section small left">' +
+                                '<p class="section-title">Planet Organic</p>' +
+                                '<a class="section-info" target="_blank" href="http://www.planetorganic.com/blog/">' + 
+                                    '<i class="section-icon fa fa-file-o"></i>Visit Website</a>' +
+                           '</section>' +
+                           '<section class="section small right">' +
+                                '<p class="section-title">Organaholic</p>' +
+                                '<a class="section-info" target="_blank" href="http://organaholic.com/">' + 
                                     '<i class="section-icon fa fa-file-o"></i>Visit Website</a>' +
                            '</section>';
 
