feddd4bb310ce2fb48be5c948fdcf0cb00ed0c82
Alena Ruprecht <alena@brandwatch.com>
2014-02-26 22:15:58 +0000

    green energy modal content

--- js/HouseView/GreenEnergyView.js
+++ js/HouseView/GreenEnergyView.js
@@ -12,9 +12,23 @@ function(
 
         render: function(){
             var template = '<section class="section small left">' +
-                                '<p class="section-title">Ecotricity</p>' +
-                                '<i class="section-icon fa fa-star"></i>' +
+                                '<p class="section-title"><img class="company-logo ecotricity"></img>Ecotricity</p>' +
                                 '<a class="section-info" target="_blank" href="http://www.ecotricity.co.uk/">' + 
+                                    '<i class="section-icon fa fa-file-o"></i>Visit Website</a>' +
+                           '</section>' +
+                           '<section class="section small right">' +
+                                '<p class="section-title"><img class="company-logo good-energy"></img>Good Energy</p>' +
+                                '<a class="section-info" target="_blank" href="http://www.goodenergy.co.uk/">' + 
+                                    '<i class="section-icon fa fa-file-o"></i>Visit Website</a>' +
+                           '</section>' +
+                           '<section class="section small left">' +
+                                '<p class="section-title"><img class="company-logo green-energy"></img>Green Energy</p>' +
+                                '<a class="section-info" target="_blank" href="http://www.greenenergy.uk.com/">' + 
+                                    '<i class="section-icon fa fa-file-o"></i>Visit Website</a>' +
+                           '</section>' +
+                           '<section class="section small right">' +
+                                '<p class="section-title"><img class="company-logo woodland"></img>Woodland Trust Energy</p>' +
+                                '<a class="section-info" target="_blank" href="http://www.woodlandtrustenergy.com/">' + 
                                     '<i class="section-icon fa fa-file-o"></i>Visit Website</a>' +
                            '</section>';
 
