55596bdca8244d1bd71c61ff4179a320aa2d7feb
Alena Ruprecht <alena@brandwatch.com>
2014-02-27 21:09:36 +0000

    added videoview content

--- js/VideosView.js
+++ js/VideosView.js
@@ -1,5 +1,6 @@
 define([
-    'lib/backbone'
+    'lib/backbone',
+    'lib/jquery.film_roll'
     ], 
 function(
     Backbone
@@ -10,12 +11,27 @@ function(
         
         className: 'Content VideosView',
 
-        initialize: function(){
-
-        }, 
-
         render: function(){
-            this.$el.html('Videos');
+            var template = '<div class="video-carousel">' +
+                                '<div class="video-single"><iframe width="560" height="315" src="http://www.youtube.com/embed/WfGMYdalClU" frameborder="0"></iframe></div>' +
+                                '<div class="video-single"><iframe width="560" height="315" src="http://www.youtube.com/embed/zORv8wwiadQ" frameborder="0"></iframe></div>' +
+                                '<div class="video-single"><iframe width="560" height="315" src="http://player.vimeo.com/video/1709110" frameborder="0"></iframe></div>' +
+                                '<div class="video-single"><iframe width="560" height="315" src="http://www.youtube.com/embed/9GorqroigqM" frameborder="0"></iframe></div>' +
+                                '<div class="video-single"><iframe width="560" height="315" src="http://www.youtube.com/embed/cpkRvc-sOKk" frameborder="0"></iframe></div>' +
+                           '</div>' +
+                           '<span class="video-intro">Inspirational Videos to raise awareness. Click one to play.</span>';
+
+            this.$el.html(template);
+
+            this.createCarousel();
+        },
+
+        createCarousel: function(){
+            var film_roll = new FilmRoll({
+                configure_load: true,
+                container: this.$('.video-carousel'),
+                scroll: false
+            });
         }
     });
 
