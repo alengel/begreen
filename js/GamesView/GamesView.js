f4535772ca68c15ccc3eaac845943aa1789a4d4b
Alena Ruprecht <alena@brandwatch.com>
2014-02-28 10:19:29 +0000

    KidsZone navigation

--- js/GamesView/GamesView.js
+++ js/GamesView/GamesView.js
@@ -1,7 +1,11 @@
 define([
+    'js/GamesView/QuizView',
+    'js/GamesView/KidsZoneView',
     'lib/backbone'
     ], 
 function(
+    QuizView,
+    KidsZoneView,
     Backbone
 ) {
     'use strict';
@@ -9,6 +13,11 @@ function(
     var GamesView = Backbone.View.extend({
         
         className: 'Content GamesView',
+
+        events: {
+            'click .quiz' : 'renderQuizView',
+            'click .kids-zone' : 'renderKidsZone'
+        },
 
         render: function(){
             var template =  '<div class="section-container games quiz"><span class="container-title">' +
@@ -19,6 +28,20 @@ function(
                             '</div>';
 
             this.$el.html(template);
+        },
+
+        renderQuizView: function(){
+            var quizView = new QuizView();
+
+            this.$el.html(quizView.$el);
+            quizView.render();
+        },
+
+        renderKidsZone: function(){
+            var kidsZoneView = new KidsZoneView();
+
+            this.$el.html(kidsZoneView.$el);
+            kidsZoneView.render();
         }
     });
 
