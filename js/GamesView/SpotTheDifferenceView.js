b71a79f67251ab5bcf803f6d033405ebfc36d09e
Alena Ruprecht <alena@brandwatch.com>
2014-03-01 19:26:25 +0000

    added all content for spot the difference game

--- js/GamesView/SpotTheDifferenceView.js
+++ js/GamesView/SpotTheDifferenceView.js
@@ -12,26 +12,29 @@ function(
 
         events: {
             'click .score-point' : 'addPoint',
-            'click .winner-button' : 'playAgain'
+            'click .winner-button' : 'playAgain',
+            'click .try-another': 'tryAnother'
         },
 
         initialize: function(){
             this.scoreBoard = [];
+
+            this.chooseRandomGame();
         },
 
         render: function(){
             var template = '<div class="image-container">' + 
-                                '<div class="pic image-original"></div>' + 
+                                '<div class="pic image-original-' + this.game +'"></div>' + 
                                 '<div class="pic-divider"></div>' +
-                                '<div class="pic image-false"></div>' +
+                                '<div class="pic image-false-' + this.game + '"></div>' +
                            '</div>' +
                            '<div class="score-container">' +
                                 '<p>Click where you see a difference.</p>' +
                                 '<span>Already found </span>' +
                                 '<span class="found">0</span>' +
                                 '<span class="out-of"> / </span>' +
-                                '<span class="total">5</span>' +
-                                '<div class="next-game">Try Another</div>' +
+                                '<span class="total"></span>' +
+                                '<div class="next-game try-another">Try Another</div>' +
                            '</div>';
 
             this.$el.html(template);
@@ -39,14 +42,19 @@ function(
             this.addAllDifferences();
         },
 
-        addAllDifferences: function(){
-            var differences = '<div class="diff1 score-point"></div>' +
-                              '<div class="diff2 score-point"></div>' +
-                              '<div class="diff3 score-point"></div>' +
-                              '<div class="diff4 score-point"></div>' +
-                              '<div class="diff5 score-point"></div>';
+        chooseRandomGame: function(){
+            this.game = _.random(1, 3);
+        },
 
-            this.$('.image-false').append(differences);
+        addAllDifferences: function(){
+            var differences = '<div class="diff1 pic' + this.game + ' score-point"></div>' +
+                              '<div class="diff2 pic' + this.game + ' score-point"></div>' +
+                              '<div class="diff3 pic' + this.game + ' score-point"></div>' +
+                              '<div class="diff4 pic' + this.game + ' score-point"></div>' +
+                              '<div class="diff5 pic' + this.game + ' score-point"></div>';
+
+            this.$('.total').text('5');
+            this.$('.image-false-' + this.game).append(differences);
         },
 
         addPoint: function(e){
@@ -70,7 +78,7 @@ function(
             var that = this,
                 popup = '<div class="popup winner">' +
                             '<h2><i class="fa fa-trophy winner-icon"></i>You Won!</h2>' +
-                            '<div class="next-game winner-button">Play Again?</div>' +
+                            '<div class="next-game winner-button next-game">Play Again?</div>' +
                         '</div>';
             this.$el.prepend(popup);
 
@@ -80,6 +88,17 @@ function(
         },
 
         playAgain: function(){
+            this.tryAnother();
+            this.render();
+        },
+
+        tryAnother: function(){
+            this.game += 1;
+
+            if(this.game === 4){
+                this.game = 1;
+            }
+
             this.render();
         }
     });
